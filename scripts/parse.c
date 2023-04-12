7d53b2611b0879d3f8f71911fe519ee947e47765
Andy Ragusa <aragusa@cisco.com>
2023-04-12 13:33:40 -0700

    blah

--- scripts/parse.c
+++ scripts/parse.c
@@ -56,23 +56,13 @@ done:
 
 //https://en.wikipedia.org/wiki/ISO_9660
 #define EMPTY_LEN 32768
-int parseFile(const char * const fileName){
 
-    uint8_t * data = NULL;
-    size_t dataLen = 0;
+#define VOLUME_DESCRIPTOR_SIZE 0x800
+
+int parseVolumeDescriptor(const uint8_t * const data) {
+    size_t parseIdx = 0;
+    size_t i = 0;
     int ret = -1;
-    size_t i, parseIdx = EMPTY_LEN;
-
-    if (readData(fileName, &data, &dataLen)){
-        printf("%s::%d::Unable to read file\n", __FUNCTION__, __LINE__);
-        goto done;
-    }
-    if (dataLen < EMPTY_LEN){
-        printf("%s::%d::ERROR\n", __FUNCTION__, __LINE__);
-        goto done;
-    }
-
-    printf("dataLen = %ld\n", dataLen);
 
     printf("Volume Descriptor\n");
 
@@ -108,6 +98,54 @@ int parseFile(const char * const fileName){
         parseIdx++;
     }
     printf("All zeros\n");
+
+    ret = 0;
+done:
+    return ret;
+
+
+}
+
+
+int parseFile(const char * const fileName){
+
+    uint8_t * data = NULL;
+    size_t dataLen = 0;
+    int ret = -1;
+    size_t i, parseIdx = EMPTY_LEN;
+
+    if (readData(fileName, &data, &dataLen)){
+        printf("%s::%d::Unable to read file\n", __FUNCTION__, __LINE__);
+        goto done;
+    }
+    if (dataLen < EMPTY_LEN){
+        printf("%s::%d::ERROR\n", __FUNCTION__, __LINE__);
+        goto done;
+    }
+
+    printf("dataLen = %ld\n", dataLen);
+
+
+#if 0
+    int andy = 0;
+#endif
+    for (i = EMPTY_LEN; i < dataLen; i+= VOLUME_DESCRIPTOR_SIZE){
+        if (parseVolumeDescriptor(&(data[i ]))){
+            goto done;
+        }
+
+#if 0
+        if (1 == andy){
+            break;
+        }
+        andy++;
+#endif
+    }
+
+
+
+
+
 
 
 
