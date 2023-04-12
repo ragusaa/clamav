147c951e579ac7268045a93f5966e0df2945311c
Andy Ragusa <aragusa@cisco.com>
2023-04-12 13:21:41 -0700

    blah

---
+++ scripts/parse.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int readData(const char * const fileName, uint8_t ** data, size_t * dataLen){
+    int ret = -1;
+    FILE * fp = NULL;
+    size_t bytesRead = 0;
+
+    uint8_t* dRet = NULL;
+    size_t dRetLen = 0;
+    *data = NULL;
+    *dataLen = 0;
+
+    fp = fopen(fileName, "rb");
+
+    if (NULL == fp){
+        perror("fopen");
+        goto done;
+    }
+
+    fseek(fp, 0, SEEK_END);
+
+    dRetLen = ftell(fp);
+    rewind (fp) ;
+
+    dRet = malloc(dRetLen);
+    if (NULL == dRet){
+        perror("malloc");
+        goto done;
+    }
+
+    while (bytesRead < dRetLen){
+        int ret = fread(&(dRet[bytesRead]), 1, dRetLen - bytesRead, fp);
+        if (ret > 0){
+            bytesRead += ret;
+        } else {
+            break;
+        }
+    }
+
+    if (bytesRead != dRetLen){
+        goto done;
+    }
+
+    ret = 0;
+    *data = dRet;
+    *dataLen = dRetLen;
+
+done:
+    if (ret){
+        free(dRet);
+    }
+    return ret;
+}
+
+//https://en.wikipedia.org/wiki/ISO_9660
+#define EMPTY_LEN 32768
+int parseFile(const char * const fileName){
+
+    uint8_t * data = NULL;
+    size_t dataLen = 0;
+    int ret = -1;
+    size_t i, parseIdx = EMPTY_LEN;
+
+    if (readData(fileName, &data, &dataLen)){
+        printf("%s::%d::Unable to read file\n", __FUNCTION__, __LINE__);
+        goto done;
+    }
+    if (dataLen < EMPTY_LEN){
+        printf("%s::%d::ERROR\n", __FUNCTION__, __LINE__);
+        goto done;
+    }
+
+    printf("dataLen = %ld\n", dataLen);
+
+    printf("Volume Descriptor\n");
+
+    printf("Type\n");
+    printf("%d\n", data[parseIdx]);
+    /*
+     * 0	Boot record volume descriptor
+     * 1	Primary volume descriptor
+     * 2	Supplementary volume descriptor, or enhanced volume descriptor
+     * 3	Volume partition descriptor
+     * 255	Volume descriptor set terminator
+     *
+     */
+    parseIdx += 1;
+
+    printf("Identifier\n");
+    for (i = 0; i < 5; i++){
+        printf("%c", data[parseIdx++]);
+    }
+    printf("\n");
+
+    printf("Version\n");
+    printf("%d\n", data[parseIdx]);
+    parseIdx += 1;
+
+    printf("Data\n");
+    for (i = 0; i < 2041; i++){
+        if (0 != data[parseIdx]){
+            printf("idx = '%ld', %02x\n", parseIdx, data[parseIdx]);
+            goto done;
+        }
+
+        parseIdx++;
+    }
+    printf("All zeros\n");
+
+
+
+    printf("Success\n");
+
+
+
+
+ret = 0;
+
+done:
+if (data){
+    free(data);
+}
+return ret;
+}
+
+int main(int argc, char ** argv){
+
+    int i;
+    for (i = 1; i < argc; i++){
+        parseFile(argv[i]);
+    }
+
+
+    return 0;
+}
